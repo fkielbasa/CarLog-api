2cb2dbee07d76a7b4d5e2a7f58acf3edb34b1e1c
fkielbasa <126203484+fkielbasa@users.noreply.github.com>
2024-10-27 10:31:44 +0100

    feat: add bean for secure all endpoints

---
+++ src/main/kotlin/com/example/carlog/api/config/SecurityConfig.kt
@@ -0,0 +1,21 @@
+package com.example.carlog.api.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+                .authorizeHttpRequests{request ->
+                    request
+                            .anyRequest().authenticated()
+                }
+        return http.build()
+    }
+}
