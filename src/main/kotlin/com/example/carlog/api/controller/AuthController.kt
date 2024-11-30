433719c95f486b8cadaf678e83f17b07714da1b5
fkielbasa <126203484+fkielbasa@users.noreply.github.com>
2024-11-30 23:32:43 +0100

    feat: auth controller

---
+++ src/main/kotlin/com/example/carlog/api/controller/AuthController.kt
@@ -0,0 +1,29 @@
+package com.example.carlog.api.controller
+
+import com.example.carlog.api.security.AuthRequest
+import com.example.carlog.api.security.ReqisterRequest
+import com.example.carlog.api.security.AuthService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/auth")
+class AuthController(
+        private val authService: AuthService
+) {
+    @PostMapping("/register")
+    fun register(
+            @RequestBody request: ReqisterRequest
+    ): ResponseEntity<String> {
+        return ResponseEntity.ok<String>(authService.register(request))
+    }
+    @PostMapping("/login")
+    fun authenticate(
+            @RequestBody user: AuthRequest)
+    : String {
+        return authService.authenticate(user)
+    }
+}
