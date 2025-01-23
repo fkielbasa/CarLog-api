2df1fd4b284b3215a2d0507e0695e40e208951ad
fkielbasa <126203484+fkielbasa@users.noreply.github.com>
2025-01-23 21:44:05 +0100

    feat: vehicle entity mapper

---
+++ src/main/kotlin/com/example/carlog/api/dto/VehicleMapper.kt
@@ -0,0 +1,20 @@
+package com.example.carlog.api.dto
+
+import com.example.carlog.api.model.Vehicle
+
+class VehicleMapper {
+    companion object {
+        fun mapToVehicleResponse(vehicle: Vehicle): VehicleResponse {
+            return VehicleResponse(
+                    id = vehicle.id,
+                    brand = vehicle.brand,
+                    model = vehicle.model,
+                    year = vehicle.year,
+                    vin = vehicle.vin,
+                    horsepower = vehicle.horsepower,
+                    torque = vehicle.torque,
+                    mileage = vehicle.mileage
+            )
+        }
+    }
+}
